c9886fc35cb1d9837b4cb943167132775ff14222
Joao <vtrdll3@gmail.com>
2025-06-10 23:39:21 -0300

    analise por unidade de atendimento

--- data_cleasing.py
+++ data_cleasing.py
@@ -5,7 +5,7 @@ data = pd.read_csv('2025-05-06_Sistema_E-Saude_Medicos_-_Base_de_Dados (1).csv',
 
    
 #remove as colunas que não necessárias. 
-colum = [3,5,7,9,10,11,14,15,16,18,21,22,23,24,
+colum = [3,7,9,10,14,15,16,18,21,22,23,24,
          25,26,27,28,29,30,31,32,33,34,35,36,37,39,40]
 colum_remove = [data.columns[i] for i in colum]
 
---
+++ unidade.py
@@ -0,0 +1,44 @@
+from argparse import OPTIONAL
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+
+
+data = pd.read_csv('dados_limpos.csv', sep=';', encoding= 'cp1252')
+
+#atendimentos por unidade
+contagem = data['Código da Unidade'].value_counts()
+mapa_codigos = data.drop_duplicates('Código da Unidade').set_index('Código da Unidade')['Descrição da Unidade'].to_dict()
+
+unidade = []
+total = []
+
+for codigo, quantidade in contagem.head(20).items():
+    descricao = mapa_codigos.get(codigo, 'Descrição não encontrada')
+    print(f"Código: {codigo} - {descricao} - Quantidade: {quantidade}")
+
+    unidade.append(descricao)
+    total.append(quantidade)
+
+
+plt.figure(figsize=(12, 8))
+plt.barh(unidade, total, color='skyblue')
+plt.xlabel('Quantidade')
+plt.title('Top 20 Unidades por ATENDIMENTO')
+plt.gca().invert_yaxis()  
+
+for i, v in enumerate(total):
+    plt.text(v + max(total)*0.01, i, str(v), va='center')
+
+plt.tight_layout()
+plt.show()
+
+
+
+'''
+contagem_df = data.groupby('Descrição da Unidade')['Desencadeou Internamento'].value_counts().unstack(fill_value=0)
+top20_sim = contagem_df.sort_values(by='Nao', ascending=False).head(20)
+
+top20_sim.plot(kind='barh', stacked=True, figsize=(10, 8))
+
+plt.show()'''
